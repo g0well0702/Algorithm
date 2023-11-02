25466f8b7937d0eb083b20dd358df3ab1a74353c
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2023-11-03 00:58:41 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0);
+    int n = 0;
+    cin >> n;
+    
+    for(int i = n-1; i >= 1; i--){
+        for(int j = n-1; j > i; j--) cout << ' ';
+        for(int j = 1; j <= 2*i+1; j++) cout << '*';
+        cout << '\n';
+    }
+    
+    int i = 1;
+    for(; i<=n-1; i++) cout << ' ';
+    for(; i<=n; i++) cout << '*';
+    cout << '\n';
+    
+    for(int i = 1; i <= n-1; i++){
+        for(int j = n-1; j > i; j--) cout << ' ';
+        for(int j = 1; j <= 2*i+1; j++) cout << '*';
+        cout << '\n';
+    }
+}
