4df3ca0c9f01d7fc5001665d4d99bb526d47bda4
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2023-11-02 21:19:33 +0900

    Update 알파벳 개수.cc

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -6,7 +6,7 @@ int main(){
     string word;
     int arr[26] = {0}; // 지역변수이기 때문에 0으로 초기화 해줘야 한다. 혹은 fill(arr, arr+26, 0); 을 통해서도 가능
     cin >> word; // string 을 cin 으로 바로 입력받을 수 있다
-    for(int c : word){ // 이런 식으로 순회할 수 있다
+    for(auto c : word){ // 이런 식으로 순회할 수 있다
         arr[c - 'a']++;
     }
     for(int i=0; i<26; i++)
