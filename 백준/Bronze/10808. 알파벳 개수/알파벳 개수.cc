68fe36720cc8a0cdc3a0938a95ec7c2ac5850392
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2023-11-02 21:06:15 +0900

    Update 알파벳 개수.cc

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -4,9 +4,9 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0), cin.tie(0);
     string word;
-    int arr[26] = {0};
-    cin >> word;
-    for(int c : word){
+    int arr[26] = {0}; // 지역변수이기 때문에 0으로 초기화 해줘야 한다. 혹은 fill(arr, arr+26, 0); 을 통해서도 가능
+    cin >> word; // string 을 cin 으로 바로 입력받을 수 있다
+    for(int c : word){ // 이런 식으로 순회할 수 있다
         arr[c - 'a']++;
     }
     for(int i=0; i<26; i++)
