d885dd11d9f508022239bb054b5b4720f9f485be
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2023-11-03 00:25:31 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -15,9 +15,9 @@ int main(){
     for(int i = 1; i <= 2*n-1; i++) cout << '*'; // 중간에 별이 9개 찍혀야 한다
     cout << '\n';
     
-    for(int i = 1; i <= n-1; i++){
-        for(int j = 1; j <= i; j++) cout << ' '; // i가 1씩 증가할 때마다 공백은 한 칸씩 증가한다.
-        for(int j = 1; j <=2*n-2*i-1 ; j++) cout << '*'; // i가 1씩 증가할 때마다 두 칸씩 감소한다
+    for(int i = n-1; i >= 1; i--){
+        for(int j = 1; j <= n-i; j++) cout << ' '; // i가 1씩 감소할 때마다 공백은 한 칸씩 증가한다.
+        for(int j = 1; j <= 2*i-1; j++) cout << '*'; // i가 1씩 감소할 때마다 두 칸씩 감소한다
         cout << '\n';
     }
 }
