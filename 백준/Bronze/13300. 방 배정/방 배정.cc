fcc0f710dac61795c7ddfb86ccb21e06360cb074
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2023-11-04 18:03:30 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr0[6];
+int arr1[6];
+int main(){
+    int n, k, s, g, count = 0;
+    cin >> n >> k;
+    
+    for(int i =0; i <n; i++){
+        cin >> s >> g;
+        if(s == 0) arr0[g-1]++;
+        else arr1[g-1]++;
+    }
+    
+    for(int i= 0; i<n; i++){
+        if(arr0[i] % k == 0) count += arr0[i] / k;
+        else count += (arr0[i] / k) + 1;
+    }
+    
+    for(int i= 0; i<n; i++){
+        if(arr1[i] % k == 0) count += arr1[i] / k;
+        else count += (arr1[i] / k) + 1;
+    }
+    cout << count;
+}
