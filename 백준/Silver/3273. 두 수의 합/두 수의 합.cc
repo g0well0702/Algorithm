34381c744fe88bff53a3a79dc9ce23ebc86dfefd
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2023-11-04 11:52:56 +0900

    Update 두 수의 합.cc

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -13,9 +13,12 @@ int main() {
         cin >> arr1[i];
 
     cin >> x;
+    //틀린 답안 : 여기서 x 값을 배열의 값들과 계산하게 되는 경우. 당연히 배열의 값이 x 값보다 클 수는 있음
+    
+    
  
     for (int i = 0; i < n; i++) {
-        if (arr1[i] < x && arr2[x - arr1[i]] > 0) {
+        if (arr1[i] < x && arr2[x - arr1[i]] > 0) { // arr1[i] < x 이 조건이 필수, 왜냐면 x 가 더 작을 때는 만족하는 쌍이 없기 때문에
             count += arr2[x-arr1[i]];
         }
         arr2[arr1[i]]++;
