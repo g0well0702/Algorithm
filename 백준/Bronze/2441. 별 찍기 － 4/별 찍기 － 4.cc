5d7e3d738ba24dde17e17cd80a09dd1e28e8d2bc
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2024-04-07 18:51:45 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0);
+    int n = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int j = 0;
+        for(; j < i; j++){
+            cout << ' ';
+        }
+        for(; j < n; j++){
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
