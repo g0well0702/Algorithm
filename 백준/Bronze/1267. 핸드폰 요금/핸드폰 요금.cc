08e0d887373dbe784f0f08d381677ef8a1589c7b
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2024-04-07 17:54:11 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -1,22 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
-    int n = 0;
-    int arr[20] = { 0 };
-    int minsik_sum = 0;
-    int youngsik_sum = 0;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, x, Y = 0, M = 0;
+    
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    
+    for(int i = 0; i < n; i++){
+        cin >> x;
+        Y += 10 + (x / 30) * 10;
+        M += 15 + (x / 60) * 15;
     }
-    for (int i = 0; i < n; i++) {
-        youngsik_sum += (arr[i] / 30 * 10) + 10;
-        minsik_sum += (arr[i] / 60 * 15) + 15;
-    }
-
-    if (minsik_sum > youngsik_sum)  cout << 'Y' << ' ' << youngsik_sum;
-    else if (minsik_sum == youngsik_sum)  cout << 'Y' << ' ' << 'M' << ' ' << minsik_sum;
-    else  cout << 'M' << ' ' << minsik_sum;
+    
+    if(Y > M) cout << 'M' << ' ' << M;
+    else if(Y == M) cout << 'Y' << ' ' << 'M' << ' ' << Y;
+    else cout << 'Y' << ' ' << Y;
+    
 }
