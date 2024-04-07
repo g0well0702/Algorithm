897b2e63239cd14367a411e44f0bae2ddcde3c73
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2024-04-07 16:52:36 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,38 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int arr[9];
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int arr[9] = {0};
     int sum = 0;
-    for(int i=0;i<9;i++){
+    
+    // 무조건 일곱 난쟁이가 존재한다고 했으니까 일단 다 더하고 가짜 두 놈 빼는 식으로 접근
+    for(int i = 0; i < 9; i++){
         cin >> arr[i];
         sum += arr[i];
     }
-    int t = 0;
-    for(int i=0;i<8;i++){
-        for(int j=1;j<9;j++){
-            if(sum-(arr[i] + arr[j]) == 100){
-                t= arr[i];
-                arr[i] = arr[8];
-                arr[8] = t;
-                
-                t= arr[j];
-                arr[j] = arr[7];
-                arr[7] = t;
+    
+    for(int i = 0; i < 8; i++){
+        for(int j = i + 1; j < 9; j++){
+            if(sum - (arr[i] + arr[j]) == 100){ // 가짜 두 놈 찾으면 제일 뒤로 보내기
+                swap(arr[i], arr[7]);
+                swap(arr[j], arr[8]);
             }
         }
     }
-    int tmp = 0;
-    for(int i = 6; i > 0; i--){
-        for(int j = 0; j < i; j++){
-            if(arr[j]> arr[j+1]){
-                tmp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = tmp;
-            }
-        }
-    }
+    
+    sort(arr, arr + 7); // 제일 뒤 두 놈은 가짜니까 그 전까지 sort
+    
     for(int i = 0; i < 7; i++){
         cout << arr[i] << '\n';
     }
+    
 }
