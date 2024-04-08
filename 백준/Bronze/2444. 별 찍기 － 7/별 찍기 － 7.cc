541a8005a16ca6074056a02013ae3bd092e020c6
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2024-04-09 00:13:04 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -1,23 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main(void){
     ios::sync_with_stdio(0), cin.tie(0);
-    int n = 0;
+    
+    int n;
     cin >> n;
     
-    for(int i = 1; i <= n-1; i++){
-        for(int j = 1; j <= n-i; j++) cout << ' '; // i 가 1씩 증가할 때마다 공백은 한 칸씩 줄어든다
-        for(int j = 1; j <= 2*i-1; j++) cout << '*'; // i 가 1씩 증가할 때마다 별이 두개씩 증가한다
+    for(int i = 1; i <= n - 1; i++){
+        for(int j = 1; j <= n - i; j++){
+            cout << ' ';
+        }
+        for(int j = 1; j <= 2 * i - 1; j++){
+            cout << '*';
+        }
         cout << '\n';
     }
     
-    for(int i = 1; i <= 2*n-1; i++) cout << '*'; // 중간에 별이 9개 찍혀야 한다
+    for(int i = 1; i <= 2 * n - 1; i++){
+        cout << '*';
+    }
     cout << '\n';
     
-    for(int i = n-1; i >= 1; i--){
-        for(int j = 1; j <= n-i; j++) cout << ' '; // i가 1씩 감소할 때마다 공백은 한 칸씩 증가한다.
-        for(int j = 1; j <= 2*i-1; j++) cout << '*'; // i가 1씩 감소할 때마다 두 칸씩 감소한다
+    for(int i = n - 1; i > 0; i--){
+        for(int j = n; j > i; j--){
+            cout << ' ';
+        }
+        for(int j = 1; j <= 2 * i - 1; j++){
+            cout << '*';
+        }
         cout << '\n';
     }
+    
+    
 }
