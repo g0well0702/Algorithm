a658dad81439a455437e5375a741e7ca31ca119e
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2024-04-09 10:37:21 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -1,25 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main(void){
     ios::sync_with_stdio(0), cin.tie(0);
-    int n = 0;
+    
+    int n;
     cin >> n;
     
-    for(int i = n-1; i >= 1; i--){
-        for(int j = n-1; j > i; j--) cout << ' ';
-        for(int j = 1; j <= 2*i+1; j++) cout << '*';
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j < i; j++){
+            cout << ' ';
+        }
+        for(int j = i; j <= 2 * n - i; j++){
+            cout << '*';
+        }
         cout << '\n';
     }
     
-    int i = 1;
-    for(; i<=n-1; i++) cout << ' ';
-    for(; i<=n; i++) cout << '*';
-    cout << '\n';
-    
-    for(int i = 1; i <= n-1; i++){
-        for(int j = n-1; j > i; j--) cout << ' ';
-        for(int j = 1; j <= 2*i+1; j++) cout << '*';
+    for(int i = n - 1 ; i > 0; i--){
+        for(int j = 1; j < i; j++){
+            cout << ' ';
+        }
+        for(int j = i; j <= 2 * n - i; j++){
+            cout << '*';
+        }
         cout << '\n';
     }
 }
