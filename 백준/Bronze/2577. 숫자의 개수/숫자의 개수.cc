c5326f213aaf12da3734807f57a69bff709bb6f8
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2024-04-12 21:17:12 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -6,12 +6,12 @@ int arr[10];
 int main(void){
     ios::sync_with_stdio(0), cin.tie(0);
     
-    int a, b, c, result;
+    int a, b, c;
     cin >> a >> b >> c;
     
     string s = to_string(a * b * c);
     
-    for(auto c : s)
+    for(char c : s)
         arr[c - '0']++;
     
     for(int i = 0; i < 10; i++)
