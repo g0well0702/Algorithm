543f93a4daf39716ec17ffe812cb6a43c4627dca
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2024-04-15 13:38:26 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -1,41 +1,38 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-string s1[1000];
-string s2[1000];
-int freq1[26];
-int freq2[26];
-
-int can_strfry(string first, string second) {
-    int count = 0;
-    if (first.length() != second.length() || first.length() < 0 || second.length() < 0) return 0;
-    for (int i = 0; i < first.length(); i++) {
-        freq1[first[i] - 'a']++;
-    }
-    for (int i = 0; i < second.length(); i++) {
-        freq2[second[i] - 'a']++;
-    }
-    for (int i = 0; i < 26; i++) {
-        if (freq1[i] != freq2[i]) count++;
-    }
-    if (count == 0) return 1;
-    return 0;
-}
-
-int main() {
+int main(void){
     ios::sync_with_stdio(0), cin.tie(0);
-    int n = 0;
+    
+    int n;
+    int count[1000] = {0};
+    string s1, s2;
     cin >> n;
-    if (n < 0 || n > 1001) return 0;
-    for (int i = 0; i < n; i++) {
-        cin >> s1[i] >> s2[i];
-    }
-    for (int i = 0; i < n; i++) {
-        if (can_strfry(s1[i], s2[i])) cout << "Possible" << '\n';
-        else cout << "Impossible" << '\n';
+    
+    for(int i = 0; i < n; i++){
         
-        fill(freq1, freq1+26, 0);
-        fill(freq2, freq2+26, 0);
+        int arr1[26] = {0}, arr2[26] = {0};
+        cin >> s1 >> s2;
+        if(s1.size() != s2.size()) continue;
+        
+        for(int i = 0; i < s1.size(); i++){
+            arr1[s1[i] - 'a']++;
+            arr2[s2[i] - 'a']++;
+        }
+        
+        int flag = 0;
+        
+        for(int j = 0; j < 26; j++){
+                if(arr1[j] == arr2[j]) flag++;
+        }
+        
+        if(flag == 26) count[i] = 1;
+        else count[i] = 0;
     }
-    return 0;
+    
+    for(int i = 0; i < n; i++){
+        if(count[i] == 1) cout << "Possible" << '\n';
+        else cout << "Impossible" << '\n';
+    }
+    
 }
