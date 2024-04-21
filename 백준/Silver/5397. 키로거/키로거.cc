903cd801bc74da1549c733ef31e7b84fa518f32f
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2024-04-21 14:31:34 +0900

    [Silver II] Title: 키로거, Time: 260 ms, Memory: 73528 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.cc
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -17,7 +17,7 @@ int main(void) {
         for (int j = 0; j < s.size(); j++) {
             if (s[j] == '<') {
                 auto tempCursor = cursor;
-                if (*(--tempCursor) != '\n') {
+                if (cursor != L.begin() && *(--tempCursor) != '\n') {
                     --cursor;
                 }
             }
@@ -27,7 +27,7 @@ int main(void) {
             }
             else if (s[j] == '-') {
                 auto tempCursor = cursor;
-                if (*(--tempCursor) != '\n') {
+                if (cursor != L.begin() && *(--tempCursor) != '\n') {
                     --cursor;
                     cursor = L.erase(cursor);
                 }
