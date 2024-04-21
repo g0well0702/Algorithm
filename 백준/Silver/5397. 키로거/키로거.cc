ecaf437dbfc59aceafaea843b3ad1871d180ca5e
g0well0702 <136082638+g0well0702@users.noreply.github.com>
2024-04-21 14:24:43 +0900

    [Silver II] Title: 키로거, Time: 300 ms, Memory: 73528 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    list<char> L = {};
+    list<char>::iterator cursor = L.end();
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < s.size(); j++) {
+            if (s[j] == '<') {
+                auto tempCursor = cursor;
+                if (cursor != L.begin() && *(--tempCursor) != '\n') {
+                    --cursor;
+                }
+            }
+            else if (s[j] == '>') {
+                if(cursor != L.end())
+                    ++cursor;
+            }
+            else if (s[j] == '-') {
+                auto tempCursor = cursor;
+                if (cursor != L.begin() && *(--tempCursor) != '\n') {
+                    --cursor;
+                    cursor = L.erase(cursor);
+                }
+            }
+            else {
+                L.insert(cursor, s[j]);
+            }
+        }
+        cursor = L.end(); // 커서를 마지막으로 초기화
+        L.insert(cursor, '\n');
+    }
+
+    for (auto c : L) cout << c;
+}
